6395816f875372573e2202da48b4d7e8d676459b
João Victor Oliveira Couto <commits@jictyvoo.dev>
2022-09-08 21:30:52 -0300

    :ambulance: feat: Fix `CNH` validator algorithm
    - Add test cases to check if it's correct now

--- validators/cnh_validator.go
+++ validators/cnh_validator.go
@@ -15,13 +15,13 @@ func CNH(element string) (result error) {
 	)
 
 	for _, character := range element {
+		actualNumber := character - '0'
 		if iterations < LengthCPF-2 {
-			actualNumber := character - '0'
 			{
 				verifierDigits[0] += (actualNumber) * weights[0]
 				verifierDigits[1] += (actualNumber) * weights[1]
-				weights[0]--
 				weights[1]++
+				weights[0]--
 			}
 
 			if repeatedNumber[0] == -1 {
@@ -33,30 +33,22 @@ func CNH(element string) (result error) {
 				repeatedNumber[1]++
 			}
 		} else {
-			originalVerifier = append(originalVerifier, character-'0')
+			originalVerifier = append(originalVerifier, actualNumber)
 		}
 		iterations++
 	}
 
 	// start checking the digits
 	var shouldRecalculate bool
-	if verifierDigits[0] %= 11; verifierDigits[0] > 9 {
+	if verifierDigits[0] %= 11; verifierDigits[0] >= 10 {
 		verifierDigits[0] = 0
 		shouldRecalculate = true
 	}
 
-	verifierDigits[1] %= 11
-	if shouldRecalculate {
-		checkValue := verifierDigits[1] - 2
-		if checkValue < 0 {
-			verifierDigits[1] += 9
-		} else {
-			verifierDigits[1] -= 2
-		}
-	}
-	// final adjust on second digit
-	if verifierDigits[1] > 9 {
+	if verifierDigits[1] %= 11; verifierDigits[1] >= 10 {
 		verifierDigits[1] = 0
+	} else if shouldRecalculate {
+		verifierDigits[1] -= 2
 	}
 
 	hasIncorrectLength := repeatedNumber[1] >= LengthCNH-2 || iterations > LengthCNH || len(originalVerifier) != 2
--- validators/cnh_validator_test.go
+++ validators/cnh_validator_test.go
@@ -2,9 +2,19 @@ package validators
 
 import "testing"
 
+func _getValidCNH() [14]string {
+	return [...]string{
+		"22522791500", "46613298880", "18271939762", "03621746707", "26606066255", "33235836407", "35318223990",
+		"96195156706", "33004095866", "87672878823", "33282790801", "29231325340", "62504334773", "37882306567",
+	}
+}
+
 func TestCNH(t *testing.T) {
-	const value = "22522791500"
-	if err := CNH(value); err != nil {
-		t.Error(err)
+	testCases := _getValidCNH()
+	for _, testCase := range testCases {
+		resultErr := CNH(testCase)
+		if resultErr != nil {
+			t.Error(resultErr)
+		}
 	}
 }
