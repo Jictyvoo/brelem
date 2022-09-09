9e84b4f4592c5185052a8c4075d79d23df5780ea
João Victor Oliveira Couto <commits@jictyvoo.dev>
2022-09-09 18:42:51 -0300

    :pager: feat: Add new `documents` package
    This new package holds all structs for brazilian documents

--- utils/errors.go
+++ utils/errors.go
@@ -6,4 +6,5 @@ var (
 	ErrEmptyString            = errors.New("empty string")
 	ErrInvalidElement         = errors.New("given element is invalid using the requested validator")
 	ErrElementIncorrectLength = errors.New("given element has not a correct length")
+	ErrExpiredDocument        = errors.New("document is already expred")
 )
