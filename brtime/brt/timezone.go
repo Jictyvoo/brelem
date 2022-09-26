f5304fd1c5ab15742892f31d61513d3e8492cd19
João Victor Oliveira Couto <commits@jictyvoo.dev>
2022-09-26 20:14:33 -0300

    :watch: feat: Add timezone BRT-0 converter

--- brtime/brt/timezone.go
+++ brtime/brt/timezone.go
@@ -4,6 +4,25 @@ import "time"
 
 const LocationName = "America/Sao_Paulo"
 
+// BatchConvertToBRT receives a list of addresses and sets the timezone to BRT-0 (which is UTC-3) in every element
+func BatchConvertToBRT(timestamps ...*time.Time) bool {
+	if utc3Timezone, err := time.LoadLocation(LocationName); err == nil {
+		for _, element := range timestamps {
+			// It's allowed to receive nil pointers, so check it
+			if element != nil {
+				*element = element.In(utc3Timezone)
+			}
+		}
+		return true
+	}
+	return false
+}
+
+// AsBRT receives a timestamp and then return a new time.Time in UTC-3 (BRT-0)
+func AsBRT(timestamp time.Time) time.Time {
+	return timestamp.In(brtLocation)
+}
+
 func Format(element time.Time) string {
 	// Check if it has hour/minute/second
 	if element.Hour() > 0 || element.Minute() > 0 || element.Second() > 0 {
